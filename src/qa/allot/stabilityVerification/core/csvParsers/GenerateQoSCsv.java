ee830079a1ae13353110b044b0ca3fc8ca300b07
vbronshtein <vbrinshtein@allot.com>
2018-08-03 18:16:00 +0300

    Fix qoscsv pipe issue

--- src/qa/allot/stabilityVerification/core/csvParsers/GenerateQoSCsv.java
+++ src/qa/allot/stabilityVerification/core/csvParsers/GenerateQoSCsv.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import qa.allot.stabilityVerification.core.trafficStreams.QosTable;
 
 /**
- * The "GenerateQoSCsv" class generate csv with Qos catalogs for stability test 
+ * The "GenerateQoSCsv" class generate csv with Qos catalogs for stability test
  * 
  * @author vbronshtein
  *
@@ -17,32 +17,19 @@ public class GenerateQoSCsv {
 	private String csvOutFile = "outputfiles/QoSCatalogs.csv";
 	private String line = "\n";
 	private String cvsSplitBy = ",";
-	
-	
-	
 
 	public String getCsvOutFile() {
 		return csvOutFile;
 	}
 
-
-
-
 	public void setCsvOutFile(String csvOutFile) {
 		this.csvOutFile = csvOutFile;
 	}
 
-
-
-
 	/**
 	 * createQoSCsv method generate csv with qos values for stability test :
 	 * 
-	 * Pipe_Max_Qos
-	 * vc_Qos_Mb_Pri1
-	 * vc_Qos_Mb_Pri2
-	 * vc_Qos_Mb_Pri3
-	 * vc_Qos_Mb_Pri4
+	 * Pipe_Max_Qos vc_Qos_Mb_Pri1 vc_Qos_Mb_Pri2 vc_Qos_Mb_Pri3 vc_Qos_Mb_Pri4
 	 * 
 	 * @param qosTable
 	 */
@@ -59,21 +46,21 @@ public class GenerateQoSCsv {
 			}
 			// Generate CSVs records per each instance
 			out.write("Pipe_Max_Qos" + cvsSplitBy);
-			out.write(qosTable.getPipeMaxQos() + cvsSplitBy);
+			out.write(qosTable.getPipeCatalogMaxQos() + cvsSplitBy);
 			out.write(line);
 
 			out.write("vc_Qos_Mb_Pri1" + cvsSplitBy);
 			out.write(qosTable.getVcCatalogQosMbPri1() + cvsSplitBy);
 			out.write(line);
-			
+
 			out.write("vc_Qos_Mb_Pri2" + cvsSplitBy);
 			out.write(qosTable.getVcCatalogQosMbPri2() + cvsSplitBy);
 			out.write(line);
-			
+
 			out.write("vc_Qos_Mb_Pri3" + cvsSplitBy);
 			out.write(qosTable.getVcCatalogQosMbPri3() + cvsSplitBy);
 			out.write(line);
-			
+
 			out.write("vc_Qos_Mb_Pri4" + cvsSplitBy);
 			out.write(qosTable.getVcCatalogQosMbPri4() + cvsSplitBy);
 			out.write(line);
@@ -84,9 +71,3 @@ public class GenerateQoSCsv {
 	}
 
 }
-
-
-
-
-
-
